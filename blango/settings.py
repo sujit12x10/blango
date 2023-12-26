a9ef68f8da20f732cbef13946da4b67134ff00cf
Sujit Singh <sujit10x12@gmail.com>
2023-12-26 17:04:21 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -99,7 +99,8 @@ class Dev(Configuration):
         "allauth", 
         "allauth.account", 
         "allauth.socialaccount", 
-        "allauth.socialaccount.providers.google"
+        "allauth.socialaccount.providers.google",
+        "rest_framework"
     ]
 
     SITE_ID = 1
