9dee019b6ce82c4fd98d24b5840558ae7a73fe97
Sujit Singh <sujit10x12@gmail.com>
2023-12-27 16:52:23 +0000

    Finish DRF views

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -4,5 +4,5 @@ from blog.models import Post
 class PostSerializer(serializers.ModelSerializer):
   class Meta:
     model = Post
-    fields = ["__all__"]
+    fields = "__all__"
     readonly = ["modified_at", "created_at"]
